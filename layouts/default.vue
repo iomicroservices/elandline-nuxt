7f22886a0b304e834537aa992578ac7a160fcc97
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-26 05:47:54 +0000

    page insights vercel

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="gd-container font-spacegrotesk">
     <header class="fixed w-full bg-[#F1F2F4] dark:bg-slate-950 z-10">
       <MainHeader />
+      <SpeedInsights />
     </header>
     <main>
       <div class="p-9" />
@@ -20,3 +21,7 @@
   min-height: 100vh;
 }
 </style>
+
+<script setup>
+  import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
