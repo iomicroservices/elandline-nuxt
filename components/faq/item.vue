af7be2f2fc0886ff9731d991a09afcc7a1f80725
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-04-09 12:22:41 +0000

    faq

---
+++ components/faq/item.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+interface Props {
+  question: string
+  answer: string
+  order: string
+}
+
+withDefaults(defineProps<Props>(), {
+  question: 'question',
+  answer: 'answer',
+  order: "1",
+})
+</script>
+
+<template>
+    <div class="group flex flex-col gap-2 rounded-lg bg-gray-300 dark:bg-slate-800 p-4 text-gray-800 dark:text-gray-200" :tabindex="order">
+    
+    <div class="font-semibold flex cursor-pointer items-center justify-between">
+      <span> <h4>{{ question }}</h4> </span>
+      <Icon name="material-symbols:expand-circle-right-outline-rounded" size="20" class="transition-all duration-500 group-focus:rotate-90" />
+      
+
+    </div>
+
+    <div class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000">
+      {{ answer }}
+    </div>
+
+  </div>
+</template>
