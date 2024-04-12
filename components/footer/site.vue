bf0ace8704fdd9aeaa52aad60b0ff262f7464647
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-04-12 00:50:20 +0000

    about

--- components/footer/site.vue
+++ components/footer/site.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <p class="text-black dark:text-zinc-300   text-base py-1 font-semibold">
-      About This Site
+      eLandline
     </p>
     <p class="py-2 dark:text-zinc-300 ">
-      This is a personal blog site built with Nuxt3, TailwindCSS, NuxtContent, Nuxt Icon. Currently it's deployed in Vercel.
+      We're passionate about providing UK businesses with virtual landline numbers that keep them connected to their customers and working smarter.
     </p>
   </div>
 </template>
