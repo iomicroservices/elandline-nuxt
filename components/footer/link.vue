35c7e01b8814e73387ac2e27e33ae880b036980a
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-04-25 20:45:05 +0000

    footer links margins

--- components/footer/link.vue
+++ components/footer/link.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex flex-col dark:text-zinc-300   my-5 md:my-0 md:justify-self-center">
-    <p class="text-black dark:text-zinc-300  text-base font-semibold">
+  <div class="flex flex-col dark:text-zinc-300 my-5 md:my-0 md:justify-self-center">
+    <p class="text-black dark:text-zinc-300 text-base font-semibold">
       Quick Links
     </p>
-    <NuxtLink to="/" class="hover:underline">
+    <NuxtLink to="/" class="hover:underline my-1">
       Home
     </NuxtLink>
-    <NuxtLink to="/blogs" class="hover:underline">
+    <NuxtLink to="/blogs" class="hover:underline my-1">
       Blogs
     </NuxtLink>
-    <NuxtLink to="/categories" class="hover:underline">
+    <NuxtLink to="/categories" class="hover:underline my-1">
       Categories
     </NuxtLink>
-    <NuxtLink to="/about" class="hover:underline">
+    <NuxtLink to="/about" class="hover:underline my-1">
       About Me
     </NuxtLink>
   </div>
