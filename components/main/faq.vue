642df6dcf0aa05dd3577fe7f7786c7c5115175e7
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-04-26 00:05:40 +0000

    faq order all 0

--- components/main/faq.vue
+++ components/main/faq.vue
@@ -6,24 +6,24 @@
             Frequently Asked Questions
         </h2>
         
-        <FaqItem order="1" question="How much is an eLandline number?" answer="It's only £11 a month. It comes with lots of free features."/>
-        <FaqItem order="2" question="I already have a landline number, can I transfer it to eLandline?" answer="Yes you can transfer it to us. Just place an order for a new number, then email us the number you'd like to transfer and we'll begin the transfer process. You'll be asked to upload your proof of ownership (recent bill), proof of address, and proof of identity to enable us to transfer it."/>
-        <FaqItem order="3" question="Do you have a landline number for my area code?" answer="Yes, we have 100% UK coverage, so you can get a number for any town or city."/>
-        <FaqItem order="4" question="Can I get a national number instead of a local number?" answer="Yes, we also provide 0330 national numbers which are the same price and are the same as local rate number to call. Just select 0330 in the area code dropdown when you order."/>
-        <FaqItem order="5" question="Can I get numbers for multiple areas and divert to the same mobile?" answer="Yes, it's £11 a month per landline number." />
-        <FaqItem order="6" question="Do you charge for diverting calls to my mobile?" answer="No. We don't charge for diverting calls, it's all free and unlimited. All you pay is £11 a month." />
-        <FaqItem order="7" question="Is it cheaper for callers to call me?" answer="Yes, it will be cheaper for callers to call your landline number instead of your mobile number." />
-        <FaqItem order="8" question="Can I cancel at any time for free?" answer="Yes, you can cancel at any time for free. We don't tie you into any long term contracts. Click the 'Cancel' link in your Control Panel to cancel or email us." />
-        <FaqItem order="9" question="Can I choose my eLandline number?" answer="You can choose the telephone area code for the town or city you'd like. The rest of the digits are assigned automatically by our system so we are unable to choose them." />
-        <FaqItem order="10" question="How do I answer calls to my eLandline number?" answer="All calls are automatically diverted to your mobile phone number for free so you simply answer it on your mobile." />
-        <FaqItem order="11" question="Can I change the mobile number(s) I divert to at any time?" answer="Yes, you can instantly change it in your Control Panel." />
-        <FaqItem order="12" question="How does the separate WhatsApp work?" answer="You set up the normal WhatsApp using your personal mobile number, and set up a separate WhatsApp made for businesses using your eLandline number. We send you instructions with download links in your welcome email after you order." />
-        <FaqItem order="13" question="Can I divert calls to multiple mobiles?" answer="Yes just add the mobile numbers you'd like to divert to in your Control Panel." />
-        <FaqItem order="14" question="Can I see the caller's number and also know it's a business call?" answer="Yes, by default you'll see your landline number on your mobile when you receive a call so you know it's a business call. You can save your landline number in your phone as a new contact and give it a name (e.g. 'Business Call' or your business name) that way whenever you get a call from your landline, that is the name that would show up on your phone." />
-        <FaqItem order="15" question="How quickly will I get my eLandline number?" answer="Your new number is automatically emailed to you immediately after you order." />
-        <FaqItem order="16" question="Can I make calls with the eLandline number?" answer="If you'd like to make calls such that your Air Landline number is displayed to your customer when they receive your call, you can use our App add-on." />
-        <FaqItem order="17" question="Can I provide for example 'Press 1 for Sales, Press 2 for Support etc' options to callers?" answer="Yes, we have a Switchboard add-on for giving options that's £4.99+VAT a month." />
-        <FaqItem order="18" question="Can I leave you and still keep my eLandline number?" answer="Yes, you can leave us and transfer your number to a new provider (e.g. BT)." />
+        <FaqItem order="0" question="How much is an eLandline number?" answer="It's only £11 a month. It comes with lots of free features."/>
+        <FaqItem order="0" question="I already have a landline number, can I transfer it to eLandline?" answer="Yes you can transfer it to us. Just place an order for a new number, then email us the number you'd like to transfer and we'll begin the transfer process. You'll be asked to upload your proof of ownership (recent bill), proof of address, and proof of identity to enable us to transfer it."/>
+        <FaqItem order="0" question="Do you have a landline number for my area code?" answer="Yes, we have 100% UK coverage, so you can get a number for any town or city."/>
+        <FaqItem order="0" question="Can I get a national number instead of a local number?" answer="Yes, we also provide 0330 national numbers which are the same price and are the same as local rate number to call. Just select 0330 in the area code dropdown when you order."/>
+        <FaqItem order="0" question="Can I get numbers for multiple areas and divert to the same mobile?" answer="Yes, it's £11 a month per landline number." />
+        <FaqItem order="0" question="Do you charge for diverting calls to my mobile?" answer="No. We don't charge for diverting calls, it's all free and unlimited. All you pay is £11 a month." />
+        <FaqItem order="0" question="Is it cheaper for callers to call me?" answer="Yes, it will be cheaper for callers to call your landline number instead of your mobile number." />
+        <FaqItem order="0" question="Can I cancel at any time for free?" answer="Yes, you can cancel at any time for free. We don't tie you into any long term contracts. Click the 'Cancel' link in your Control Panel to cancel or email us." />
+        <FaqItem order="0" question="Can I choose my eLandline number?" answer="You can choose the telephone area code for the town or city you'd like. The rest of the digits are assigned automatically by our system so we are unable to choose them." />
+        <FaqItem order="0" question="How do I answer calls to my eLandline number?" answer="All calls are automatically diverted to your mobile phone number for free so you simply answer it on your mobile." />
+        <FaqItem order="0" question="Can I change the mobile number(s) I divert to at any time?" answer="Yes, you can instantly change it in your Control Panel." />
+        <FaqItem order="0" question="How does the separate WhatsApp work?" answer="You set up the normal WhatsApp using your personal mobile number, and set up a separate WhatsApp made for businesses using your eLandline number. We send you instructions with download links in your welcome email after you order." />
+        <FaqItem order="0" question="Can I divert calls to multiple mobiles?" answer="Yes just add the mobile numbers you'd like to divert to in your Control Panel." />
+        <FaqItem order="0" question="Can I see the caller's number and also know it's a business call?" answer="Yes, by default you'll see your landline number on your mobile when you receive a call so you know it's a business call. You can save your landline number in your phone as a new contact and give it a name (e.g. 'Business Call' or your business name) that way whenever you get a call from your landline, that is the name that would show up on your phone." />
+        <FaqItem order="0" question="How quickly will I get my eLandline number?" answer="Your new number is automatically emailed to you immediately after you order." />
+        <FaqItem order="0" question="Can I make calls with the eLandline number?" answer="If you'd like to make calls such that your Air Landline number is displayed to your customer when they receive your call, you can use our App add-on." />
+        <FaqItem order="0" question="Can I provide for example 'Press 1 for Sales, Press 2 for Support etc' options to callers?" answer="Yes, we have a Switchboard add-on for giving options that's £4.99+VAT a month." />
+        <FaqItem order="0" question="Can I leave you and still keep my eLandline number?" answer="Yes, you can leave us and transfer your number to a new provider (e.g. BT)." />
     </div>
 </section>
 </template>
