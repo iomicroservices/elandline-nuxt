925562d5aea84d2c5b8aee057860c9199276daae
nurRiyad <asadnurriyad@gmail.com>
2024-08-17 17:39:58 +0600

    Update topic card component
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- components/help/card.vue
+++ components/help/card.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
 interface Props {
   title: string
-  count: number
 }
 
 withDefaults(defineProps<Props>(), {
   title: 'No title available',
-  count: 0,
 })
-
-// some random color for tags
-const color = [
-  '#dc2626',
-  '#d97706',
-  '#65a30d',
-  '#059669',
-  '#0891b2',
-  '#0284c7',
-  '#4f46e5',
-  '#7c3aed',
-  '#c026d3',
-  '#db2777',
-]
-
-// get a random number
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-const picAColor = ref(`${color.at(getRandomInt(0, 8))}`)
 </script>
 
 <template>
@@ -43,9 +18,3 @@ const picAColor = ref(`${color.at(getRandomInt(0, 8))}`)
     </NuxtLink>
   </div>
 </template>
-
-<style scoped>
-.rand-bg-color {
-  background-color: v-bind(picAColor);
-}
-</style>
